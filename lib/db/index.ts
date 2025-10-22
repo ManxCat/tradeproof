ad547101fcbcb13c9915e4006829249fd59afc6f
Andrew Shipley <andrewshipley@Andrews-MacBook.local>
2025-10-22 19:02:45 +0100

    Fix both database index files

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,7 @@
-import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import Database from 'better-sqlite3';
 import * as schema from './schema';
 
 const sqlite = new Database('tradeproof.db');
-export
+
+export const db = drizzle(sqlite, { schema });
